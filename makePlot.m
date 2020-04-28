1509c59ff5cab93c139f1809a4bfd95d865c90db
aleshinskaya <anna.leshinskaya@gmail.com>
2020-04-28 14:30:41 -0700

    added flexible color functionality

--- makePlot.m
+++ makePlot.m
@@ -2,21 +2,21 @@ function makePlot(X,FILT,names,clr)
 %makePlot(X, FILT, names, color)
 %X: column vector of data, with each column a condition to be averaged into a bar plot
 %FILT: column vector of same size as X, with 1=included and 0=excluded; filter applied column by column
-%lengths must match but filters can sum to different values per column
-%filter is POSITIVE ie 1= include, 0=exclude
-%names is a cell array
+%names: cell array of condition names, corresponding to columns in X
+% clr: optional 3-item vector of RGB values for the color desired for the bars
+% Author: Anna Leshinskaya
+% References: calls errorbar_groups by pierremegevand
+% https://www.mathworks.com/matlabcentral/fileexchange/47250-pierremegevand-errorbar_groups
 
 [n,numCond] =size(X);
 
-
-if(strcmp(clr,'yellow'))
+% default color is yellow
+if(is.empty(clr))
     clr = [249,217,10]/255;
-else
-    clr= [132,132,215]/255;
 end
 
 
-%create vector of SEs for each of the columns in X
+%create vector of standard errors for each of the columns in X
 SE = [];
 xMEAN = [];
 for c=1:numCond
